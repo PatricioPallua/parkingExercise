7b5b068961c814ecc93fe007b6603c4ed071e365
PatricioPallua <patriciopallua@hotmail.com>
2021-01-19 12:44:44 -0300

    It's ALIVEgit status!

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Places from "./Places.js"
 
 function App() {
 
+  const [chosen, setChosen] = useState("");
 
   const [small, setSmall] = useState([
       {situation: "free", vehicle: null, id:1},
@@ -41,41 +42,35 @@ function App() {
   }
 
   const occupySmall = (vehicle, event) => {
-  	console.log(event)
     var smallIndex = check(small);
     if(smallIndex >= 0){
       var newSmall = small;
       newSmall[smallIndex] = {situation: "occupied", vehicle: vehicle}
       setSmall(newSmall);
-      console.log(small);
     } else {
       occupyMedium(vehicle, event);
     }
   }
 
   const occupyMedium = (vehicle, event) => {
-  	console.log(event);
     event.preventDefault();
     var mediumIndex = check(medium);
     if(mediumIndex >= 0){
       var newMedium = medium;
       newMedium[mediumIndex] = {situation: "occupied", vehicle: vehicle, id: newMedium.id}
       setMedium(newMedium);
-      console.log(medium)
     } else {
       occupyLarge(vehicle, event);
     }
   }
 
   const occupyLarge = (vehicle, event) => {
-  	console.log(event);
     event.preventDefault();
     var largeIndex = check(large);
     if(largeIndex >= 0){
       var newLarge = large;
       newLarge[largeIndex] = {situation: "occupied", vehicle: vehicle}
       setLarge(newLarge);
-      console.log(large)
     } else {
       alert("NO PLACE FREE!");
       console.log("LA QUEUE ES: ", queue)
@@ -86,41 +81,65 @@ function App() {
     }
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    switch(chosen){
+      case "Motorcycle":
+        occupySmall(chosen, e);
+        break;
+      case "Sedan":
+        occupyMedium(chosen, e);
+        break;
+      case "Truck":
+        occupyLarge(chosen, e);
+        break;
+      default:
+        alert("NO SE UN VEHICULO VALIDO")      
+    }
+    setChosen("");
+  }
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setChosen(e.target.value);
+  }
 
   return (
     <div className="App">
       <header className="App-header">
-        <p> PARKING EXERCISE </p>
-        <button className="vehicleButton" value="Motorcycle" onClick={(event) => occupySmall("Motorcycle", event)} > MOTORCYCLE </button>
-        <button className="vehicleButton" value="Sedan" onClick={(event) => occupyMedium("Sedan", event)} > SEDAN </button>
-        <button className="vehicleButton" value="Truck" onClick={(event) => occupyLarge("Truck", event)} > TRUCK </button>                
+        <p> PARKING EXERCISE </p>             
+        <form onSubmit={handleSubmit} >
+          <select name="vehicle" onChange={handleChange} value={chosen}>
+            <option value="">Choose one...</option>
+            <option value="Motorcycle">Motorcycle</option>
+            <option value="Sedan">Sedan</option>
+            <option value="Truck">Truck</option>
+          </select>
+          <button>
+            PARK
+          </button>  
+        </form>
       </header>
       {
         <>
         <div className="small">
-          <h3 className="title">Small: </h3>
           {
             small.map(el => (
-              /*el.situtation === "free" ?
-              <div> VACIO </div>
-              :*/
-              <Places key={el.id} vehicle={el.vehicle} situation={el.situation} />
+              <Places  key={el.id} vehicle={el.vehicle} situation={el.situation} />
             ))
           }
         </div>
 
         <div className="medium">
-          <h3 className="title">Medium: </h3>
           {
             medium.map(el => (
-              <Places key={el.id} vehicle={el.vehicle} situation={el.situation} />
+              <Places  key={el.id} vehicle={el.vehicle} situation={el.situation} />
             ))
           }
         </div>
 
 
         <div className="large">
-          <h3 className="title">Large: </h3>
           {
             large.map(el => (
               <Places key={el.id} vehicle={el.vehicle} situation={el.situation} />
--- src/Places.js
+++ src/Places.js
@@ -3,16 +3,14 @@ import "./Places.css";
 
 function Places(props) {
 
-	const misterio = (props) => {
-		console.log(props)
-	}
-
 	return (
 		<div className="container" >
-			<h1> Situation: </h1>
-			<h2> {props.situation && props.situation} </h2>
-			<h1> Vehicle: </h1>
-			<h2> {props.vehicle && props.vehicle} </h2>
+			<div className={props.situation}>
+				<h1 className="title"> Situation: </h1>
+				<h2 className="prop"> {props.situation && props.situation} </h2>
+				<h1 className="title"> Vehicle: </h1>
+				<h2 className="prop"> {props.vehicle && props.vehicle} </h2>
+			</div>
 		</div>
 	)
 }
