c1ceff8c9d839edc6a11fd64b1d338ef46624d8e
PatricioPallua <patriciopallua@hotmail.com>
2021-01-20 14:51:08 -0300

    100% DONE

--- src/App.js
+++ src/App.js
@@ -41,38 +41,35 @@ function App() {
     return -1;
   }
 
-  const occupySmall = (vehicle, event) => {
+  const occupySmall = (vehicle) => {
     var smallIndex = check(small);
     if(smallIndex >= 0){
       var newSmall = small;
       newSmall[smallIndex] = {situation: "occupied", vehicle: vehicle, id: small[smallIndex].id}
       setSmall(newSmall);
     } else {
-      occupyMedium(vehicle, event);
+      occupyMedium(vehicle);
     }
   }
 
-  const occupyMedium = (vehicle, event) => {
-    event.preventDefault();
+  const occupyMedium = (vehicle) => {
     var mediumIndex = check(medium);
     if(mediumIndex >= 0){
       var newMedium = medium;
       newMedium[mediumIndex] = {situation: "occupied", vehicle: vehicle, id: medium[mediumIndex].id}
       setMedium(newMedium);
     } else {
-      occupyLarge(vehicle, event);
+      occupyLarge(vehicle);
     }
   }
 
-  const occupyLarge = (vehicle, event) => {
-    event.preventDefault();
+  const occupyLarge = (vehicle) => {
     var largeIndex = check(large);
     if(largeIndex >= 0){
       var newLarge = large;
       newLarge[largeIndex] = {situation: "occupied", vehicle: vehicle, id: large[largeIndex].id}
       setLarge(newLarge);
     } else {
-      alert("NO PLACE FREE!");
       var newQueue = queue;
       newQueue.push(vehicle)
       setQueue(newQueue);
@@ -92,7 +89,7 @@ function App() {
         occupyLarge(chosen, e);
         break;
       default:
-        alert("NO SE UN VEHICULO VALIDO")      
+        alert("NOT A VALID VEHICLE")      
     }
     setChosen("");
   }
@@ -108,30 +105,67 @@ function App() {
       for(let j=0; j<spot.length; j++){
         if(spot[j].id === i){
           spot[j] = {situation: "free", vehicle: null, id: i}
+          setSmall(spot);
         }
       }
-      setSmall(spot)
     }
     if(i >5 && i <= 10){
       let spot = [...medium];
       for(let j=0; j<spot.length; j++){
         if(spot[j].id === i){
           spot[j] = {situation: "free", vehicle: null, id: i}
+          setMedium(spot)
         }
       }
-      setMedium(spot)
     }
     if(i >10){
       let spot = [...large];
       for(let j=0; j<spot.length; j++){
         if(spot[j].id === i){
           spot[j] = {situation: "free", vehicle: null, id: i}
+          setLarge(spot)
         }
       }
-      setLarge(spot)
     }
-
   }
+
+  const handleNext = () => {
+  	switch(queue[0]){
+  		case "Motorcycle":
+  			if(check(small) !== -1 || check(medium) !== -1 || check(large) !== -1){
+  				var newQueue1 = [...queue];
+  				var vehicle1 = newQueue1.shift();
+  				occupySmall(vehicle1);
+  				setQueue(newQueue1)
+  			} else {
+  				alert("NO AVAILABLE SPACE");
+  			}
+  			break;
+  		case "Sedan":
+  			if(check(medium) !== -1 || check(large) !== -1){
+  				var newQueue2 = [...queue];
+  				var vehicle2 = newQueue2.shift();
+  				occupyMedium(vehicle2);
+  				setQueue(newQueue2)
+  			} else {
+  				alert("NO AVAILABLE SPACE");
+  			}
+  			break;
+  		case "Truck":
+  			if(check(large) !== -1){
+  				var newQueue3 = [...queue];
+  				var vehicle3 = newQueue3.shift();
+  				occupyLarge(vehicle3);
+  				setQueue(newQueue3)
+  			} else {
+  				alert("NO AVAILABLE SPACE")
+  			}
+  			break;
+  		default:
+  			alert("THE QUEUE IS EMPTY!!!")	 				
+  	}
+  }
+
 
   return (
     <div className="App">
@@ -203,6 +237,7 @@ function App() {
         		<h3> THE QUEUE IS EMPTY </h3> 
 			     :        	
 	        	<div>
+              <button onClick={handleNext}> NEXT!!! </button>
 	        		{
 	        		queue.map(el => {
 	        			return (<div> {el} </div>)
